22bb1ebaa2ed48d5863e3536ce806141ef7f90d2
theverygaming <theverygaming@users.noreply.github.com>
2025-09-13 20:28:13 +0200

    refactor stuff

--- main.py
+++ main.py
@@ -13,15 +13,14 @@ def meshtastic_stuff():
     meshtastic = meshtastic_dm.MeshtasticProto(lambda x: esplora.send_tx_packet(list(x)), channels_json)
 
     while True:
-        for _ in range(30):
-            rx = esplora.poll_rx()
-            if rx:
-                try:
-                    meshtastic.packet_rx(bytes(rx["data"]), rx["rssi"], rx["snr"])
-                except:
-                    print("exception ingesting meshtastic packet")
-                    traceback.print_exc()
-            time.sleep(0.2)
+        rx = esplora.poll_rx()
+        if rx:
+            try:
+                meshtastic.packet_rx(bytes(rx["data"]), rx["rssi"], rx["snr"])
+            except:
+                print("exception ingesting meshtastic packet")
+                traceback.print_exc()
+        time.sleep(0.2)
 
 if __name__ == "__main__":
     print("Sending config...")
