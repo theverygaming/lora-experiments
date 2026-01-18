b07aed2e86e48b33b62c83463e77d0595aaf07af
theverygaming <18639279+theverygaming@users.noreply.github.com>
2026-01-18 18:36:20 +0100

    add nix flake for easy development

---
+++ flake.nix
@@ -0,0 +1,48 @@
+{
+  description = "lora-experiments";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    sillyORM = {
+      url = "github:theverygaming/sillyORM/dev";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+      sillyORM,
+    }:
+    { }
+    // flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+      in
+      rec {
+        devShells.default = pkgs.stdenv.mkDerivation {
+          name = "lora-experiments";
+          buildInputs =
+            with pkgs;
+            [
+              python313
+
+              # mesh-python
+              python313Packages.meshtastic
+              python313Packages.cryptography
+
+              # lora_modem
+              platformio
+              # because platformio is broken idk
+              python313Packages.packaging
+            ];
+        };
+      }
+    );
+}
