4bd4d1691b054e51809df836745c4df38831101a
theverygaming <theverygaming@users.noreply.github.com>
2026-02-02 17:04:48 +0100

    random unfinished KISS stuff

--- lora_modem/src/cmdcon.cpp
+++ lora_modem/src/cmdcon.cpp
@@ -3,7 +3,6 @@
 #include <cstdint>
 #include <cstring>
 #include <main.h>
-#include <ArduinoJson.h>
 #include <variant.h>
 #include <config.h>
 
@@ -30,7 +29,7 @@ void CMDCon::set_stream(Stream *s) {
     LOG_DEBUG("set_stream");
     this->stream = s;
     radio->onReceive(rx_hook);
-    radio->modeContinousReceive();
+    radio->modeStandby();
     this->is_stby = true;
 }
 
@@ -39,229 +38,21 @@ void CMDCon::process() {
         return;
     }
     if (millis() - this->prev_telem > 2 * 1000) {
-        JsonDocument doc;
+        /*JsonDocument doc;
         doc["type"] = "telemetry";
         if (!this->is_stby) {
             doc["rssi"] = radio->rssi();
         }
-        serializeJson(doc, *this->stream);
+        serializeJson(doc, *this->stream);*/
         this->stream->println();
         this->prev_telem = millis();
     }
     dump_packets();
-    if (this->stream->available() != 0) {
-        auto jsonstr = this->stream->readStringUntil('\n');
-        jsonstr.trim();
-        JsonDocument doc;
-        DeserializationError error = deserializeJson(doc, jsonstr);
-        if (error) {
-            this->stream->print("deserializeJson() failed: ");
-            this->stream->println(error.f_str());
-            return;
-        }
-        String type = doc["type"];
-        if (type == "settings") {
-            LOG_DEBUG("got settings");
-            JsonDocument docOut;
-            if (doc["gain"].is<unsigned short>()) {
-                LOG_DEBUG("settings: set gain");
-                unsigned short gain = doc["gain"];
-                if (radio->setGain(gain)) {
-                    docOut["gain"] = gain;
-                }
-            }
+    while (this->stream->available() != 0) {
+        uint8_t byte = this->stream->read();
 
-            if (doc["txPower"].is<unsigned short>()) {
-                LOG_DEBUG("settings: set txPower");
-                unsigned short txPower = doc["txPower"];
-                if (radio->setTxPower(txPower)) {
-                    docOut["txPower"] = txPower;
-                }
-            }
-
-            if (doc["frequency"].is<unsigned long>()) {
-                LOG_DEBUG("settings: set frequency");
-                unsigned long frequency = doc["frequency"];
-                if (radio->setFrequency(frequency)) {
-                    docOut["frequency"] = frequency;
-                }
-            }
-
-            if (doc["spreadingFactor"].is<unsigned short>()) {
-                LOG_DEBUG("settings: set spreadingFactor");
-                unsigned short spreadingFactor = doc["spreadingFactor"].as<unsigned short>();
-                if (radio->setSpreadingFactor(spreadingFactor)) {
-                    docOut["spreadingFactor"] = spreadingFactor;
-                }
-            }
-
-            if (doc["signalBandwidth"].is<unsigned long>()) {
-                LOG_DEBUG("settings: set signalBandwidth");
-                unsigned long signalBandwidth = doc["signalBandwidth"].as<unsigned long>();
-                if (radio->setSignalBandwidth(signalBandwidth)) {
-                    docOut["signalBandwidth"] = signalBandwidth;
-                }
-            }
-
-            if (doc["codingRate4"].is<unsigned short>()) {
-                LOG_DEBUG("settings: set codingRate4");
-                unsigned short codingRate4 = doc["codingRate4"].as<unsigned short>();
-                if (radio->setCodingRate4(codingRate4)) {
-                    docOut["codingRate4"] = codingRate4;
-                }
-            }
-
-            if (doc["preambleLength"].is<unsigned short>()) {
-                LOG_DEBUG("settings: set preambleLength");
-                unsigned short preambleLength = doc["preambleLength"].as<unsigned short>();
-                if (radio->setPreambleLength(preambleLength)) {
-                    docOut["preambleLength"] = preambleLength;
-                }
-            }
-
-            if (doc["syncWord"].is<uint8_t>()) {
-                LOG_DEBUG("settings: set syncWord");
-                uint8_t syncWord = doc["syncWord"].as<uint8_t>();
-                if (radio->setSyncWord(syncWord)) {
-                    docOut["syncWord"] = syncWord;
-                }
-            }
-
-            if (doc["CRC"].is<bool>()) {
-                LOG_DEBUG("settings: set CRC");
-                bool CRC = doc["CRC"].as<bool>();
-                if (radio->setCRC(CRC)) {
-                    docOut["CRC"] = CRC;
-                }
-            }
-
-            if (doc["invertIQ"].is<bool>()) {
-                LOG_DEBUG("settings: set invertIQ");
-                bool invertIQ = doc["invertIQ"].as<bool>();
-                if (radio->setInvertIQ(invertIQ)) {
-                    docOut["invertIQ"] = invertIQ;
-                }
-            }
-            
-            if (doc["lowDataRateOptimize"].is<bool>()) {
-                LOG_DEBUG("settings: set lowDataRateOptimize");
-                bool lowDataRateOptimize = doc["lowDataRateOptimize"].as<bool>();
-                if (radio->setLowDataRateOptimize(lowDataRateOptimize)) {
-                    docOut["lowDataRateOptimize"] = lowDataRateOptimize;
-                }
-            }
-
-            if (doc["receive"].is<bool>()) {
-                if (doc["receive"].as<bool>()) {
-                    LOG_DEBUG("settings: set RX mode");
-                    this->is_stby = false;
-                    set_rx_mode();
-                    docOut["receive"] = true;
-                } else {
-                    LOG_DEBUG("settings: set standby mode");
-                    this->is_stby = true;
-                    set_rx_mode();
-                }
-            }
-
-            if (doc["wifi"].is<JsonObject>() && doc["wifi"]["ssid"].is<const char *>() && doc["wifi"]["password"].is<const char *>()) {
-                LOG_DEBUG("settings: got wifi SSID: %s and password", doc["wifi"]["ssid"].as<const char *>());
-                config_set_wifi(doc["wifi"]["ssid"], doc["wifi"]["password"]);
-                config_refresh();
-                docOut["wifi"]["ssid"] = doc["wifi"]["ssid"];
-                docOut["wifi"]["password"] = doc["wifi"]["password"];
-            }
-
-            serializeJson(docOut, *this->stream);
-            this->stream->println();
-        } else if (type == "metaQ") {
-            JsonDocument docOut;
-            docOut["type"] = "meta";
-            docOut["gainMax"] = radio->getGainMax();
-            docOut["txPowerMax"] = radio->getTxPowerMax();
-            serializeJson(docOut, *this->stream);
-            this->stream->println();
-        } else if (type == "packetTx") {
-            JsonArray data = doc["data"].as<JsonArray>();
-            bool cad = doc["cad"].is<bool>() && doc["cad"].as<bool>();
-            unsigned long cad_wait = doc["cadWait"].is<unsigned long>() ? doc["cadWait"].as<unsigned long>() : 1;
-            unsigned long cad_timeout = doc["cadTimeout"].is<unsigned long>() ? doc["cadTimeout"].as<unsigned long>() : 1;
-            size_t buf_size = data.size();
-            if (buf_size == 0) {
-                return;
-            }
-            LOG_DEBUG("transmitting packet with size %zu", buf_size);
-            uint8_t *buf = new uint8_t[buf_size];
-            size_t buf_idx = 0;
-            for (JsonVariant v : data) {
-                if (!v.is<unsigned int>()) {
-                    delete buf;
-                    return;
-                }
-                buf[buf_idx++] = v.as<unsigned int>();
-            }
-
-#ifdef HAS_LED
-            digitalWrite(LED_PIN, HIGH);
-#endif
-
-            const char *reason = "";
-            bool tx_allowed = true;
-            bool tx_success = false;
-
-            if (cad) {
-                LOG_DEBUG("waiting for channel to become inactive");
-                unsigned long tstart = millis();
-                while (radio->isChannelActive()) {
-                    if (millis() - tstart >= cad_timeout) {
-                        tx_allowed = false;
-                        reason = "cadTimeout";
-                        LOG_DEBUG(reason);
-                    }
-
-                    // busy. Receive and wait for cad_wait ms
-                    // we don't wanna miss any packets!
-                    set_rx_mode();
-                    unsigned long tstart2 = millis();
-                    while (millis() - tstart2 < cad_wait) {
-                        delay(1);
-                    }
-                    yield();
-                }
-            }
-
-            if (tx_allowed && radio->beginPacket()) {
-                radio->write(buf, buf_size);
-                if (radio->endPacket()) {
-                    LOG_DEBUG("successfully transmitted packet");
-                    tx_success = true;
-                } else {
-                    reason = "endPacket failed";
-                    LOG_DEBUG(reason);
-                }
-            } else {
-                reason = "beginPacket failed";
-                LOG_DEBUG(reason);
-            }
-
-            delete buf;
-            
-            LOG_DEBUG("setting radio mode back");
-            set_rx_mode();
-
-            JsonDocument docOut;
-            docOut["type"] = "txAck";
-            docOut["id"] = doc["id"];
-            docOut["success"] = tx_success;
-            if (!tx_success) {
-                docOut["reason"] = reason;
-            }
-            serializeJson(docOut, *this->stream);
-            this->stream->println();
-#ifdef HAS_LED
-            digitalWrite(LED_PIN, LOW);
-#endif
-        }
+        // end of data frame
+        if (in_frame && byte == FEND && active_command == )
     }
 }
 
@@ -270,7 +61,7 @@ void CMDCon::dump_packets() {
 #ifdef HAS_LED
         digitalWrite(LED_PIN, HIGH);
 #endif
-        JsonDocument doc;
+        /*JsonDocument doc;
         doc["type"] = "packetRx";
         doc["rssi"] = p->rssi;
         doc["snr"] = p->snr;
@@ -280,7 +71,7 @@ void CMDCon::dump_packets() {
         for (size_t i = 0; i < p->plen; i++) {
             arr.add(p->data[i]);
         }
-        serializeJson(doc, *this->stream);
+        serializeJson(doc, *this->stream);*/
         this->stream->println();
         rxQueue.readNextDone();
     }
--- lora_modem/src/cmdcon.h
+++ lora_modem/src/cmdcon.h
@@ -1,4 +1,5 @@
 #pragma once
+#include <cstdint>
 #include <stdint.h>
 #include <stddef.h>
 #include <Arduino.h>
@@ -28,6 +29,21 @@ private:
     static void rx_hook(size_t psize);
     void set_rx_mode();
     unsigned long prev_telem = 0;
+
+    // KISS frame control
+    static const uint8_t FEND = 0xC0;
+    static const uint8_t FESC = 0xDB;
+    static const uint8_t TFEND = 0xDC;
+    static const uint8_t TFESC = 0xDD;
+
+    // KISS commands
+    // https://github.com/liberatedsystems/RNode_Firmware_CE/blob/2a4d6c7a7f02a272b01c0449c728a73b9e9407af/Framing.h
+    static const uint8_t CMD_UNKNOWN = 0xFE;
+    static const uint8_t CMD_DATA = 0;
+
+    // KISS state
+    bool in_frame = false;
+    uint8_t active_command = 0;
 };
 
 extern CMDCon CMDConGlobal;
