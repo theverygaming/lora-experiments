add430652b2e2a7390b7eb84cb46e3df1f43add2
theverygaming <theverygaming@users.noreply.github.com>
2026-02-05 01:11:55 +0100

    LoRaRadio: support getting parameters

--- lora_modem/src/RadioRFM95.h
+++ lora_modem/src/RadioRFM95.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <LoRaRadio.h>
 #include <SPI.h>
+#include <cstdint>
 
 #ifndef RFM95_RESET
 #define RFM95_RESET -1
@@ -32,16 +33,40 @@ public:
     int rssi() override;
 
     bool setGain(unsigned short level) override;
+    unsigned short getGain() override;
     unsigned short getGainMax() override;
     bool setTxPower(unsigned short dbm) override;
+    unsigned short getTxPower() override;
     unsigned short getTxPowerMax() override;
     bool setFrequency(unsigned long frequencyHz) override;
+    unsigned long getFrequency() override;
     bool setSpreadingFactor(unsigned short sf) override;
+    unsigned short getSpreadingFactor() override;
     bool setSignalBandwidth(unsigned long sbw) override;
+    unsigned long getSignalBandwidth() override;
     bool setCodingRate4(unsigned short denominator) override;
+    unsigned short getCodingRate4() override;
     bool setPreambleLength(unsigned short length) override;
+    unsigned short getPreambleLength() override;
     bool setSyncWord(uint8_t sw) override;
+    uint8_t getSyncWord() override;
     bool setCRC(bool enabled) override;
+    bool getCRC() override;
     bool setInvertIQ(bool enabled) override;
+    bool getInvertIQ() override;
     bool setLowDataRateOptimize(bool enabled) override;
+    bool getLowDataRateOptimize() override;
+
+private:
+    unsigned short _gain;
+    unsigned short _txp_dbm;
+    unsigned long _frequency;
+    unsigned short _sf;
+    unsigned long _bw;
+    unsigned short _cr4;
+    unsigned short _preamble_len;
+    uint8_t _syncword;
+    bool _crc;
+    bool _invert_iq;
+    bool _low_data_rate_optimize;
 };
