c5bb7b81afb50959902b925445115a4c30f30e84
theverygaming <18639279+theverygaming@users.noreply.github.com>
2026-02-21 01:12:39 +0100

    LoraModem: airtime calculation for TX & RX

--- mesh-python/lora_modem.py
+++ mesh-python/lora_modem.py
@@ -1,4 +1,42 @@
 import dataclasses
+import math
+import logging
+
+
+_logger = logging.getLogger(__name__)
+
+
+def calculate_airtime(
+    spreading_factor: int,
+    bandwidth: int,
+    coding_rate_4: int,
+    preamble_len: int,
+    crc: bool,
+    ldro: bool,
+    with_header: bool,
+    payload_bytes: int,
+):
+    # calculations from:
+    # https://www.mobilefish.com/download/lora/lora_part17.pdf
+    # https://www.rfwireless-world.com/calculators/lorawan-airtime-calculator
+    # https://www.semtech.com/design-support/lora-calculator
+    t_sym = ((2 ** spreading_factor) / bandwidth)
+    t_preamble = (preamble_len + 4.25) * t_sym
+    payload_data_syms = 8 * payload_bytes
+    # this shit crazy lmao, I should make it more readable sometime...
+    payload_syms = (
+        8
+        + max(
+            math.ceil(
+                (payload_data_syms - (4 * spreading_factor) + 28 + (16 if crc else 0) - (0 if with_header else 20))
+                / (4 * (spreading_factor - (2 if ldro else 0)))
+            ) * (coding_rate_4),
+            0,
+        )
+    )
+    t_payload = (payload_syms) * t_sym
+    return t_preamble + t_payload
+
 
 @dataclasses.dataclass
 class LoraPacket:
@@ -11,8 +49,34 @@ class LoraPacketReceived(LoraPacket):
     freqError: int # frequency error in Hz
 
 class LoraModem:
+    def __init__(self):
+        self._spreading_factor = None
+        self._bandwidth = None
+        self._coding_rate = None
+        self._preamble_length = None
+        self._crc = None
+        self._low_data_rate_optimize = None
+
+    def _calc_airtime(self, packet: LoraPacket):
+        for a in ["_spreading_factor", "_bandwidth", "_coding_rate", "_preamble_length", "_crc", "_low_data_rate_optimize"]:
+            if getattr(self, a) is None:
+                raise Exception(f"attr '{a}' required for _calc_airtime")
+        return calculate_airtime(
+            self._spreading_factor,
+            self._bandwidth,
+            self._coding_rate,
+            self._preamble_length,
+            self._crc,
+            self._low_data_rate_optimize,
+            True,
+            len(packet.data),
+        )
+
     def start(self, rx_cb):
-        self._start(rx_cb)
+        def wrapped_rx_cb(p):
+            _logger.debug("RX airtime: %fs", self._calc_airtime(p))
+            rx_cb(p)
+        self._start(wrapped_rx_cb)
 
     def _start(self, rx_cb):
         raise NotImplementedError()
@@ -24,6 +88,7 @@ class LoraModem:
         raise NotImplementedError()
 
     def tx(self, p: LoraPacket):
+        _logger.debug("TX airtime: %fs", self._calc_airtime(p))
         self._tx(p)
 
     def _tx(self, p: LoraPacket):
@@ -42,18 +107,22 @@ class LoraModem:
         self._set_lora_params({"frequency": freq_hz})
 
     def set_spreading_factor(self, sf: int) -> None:
+        self._spreading_factor = sf
         self._set_lora_params({"spreading_factor": sf})
 
     def set_bandwidth(self, bandwidth: int) -> None:
+        self._bandwidth = bandwidth
         self._set_lora_params({"bandwidth": bandwidth})
 
     def set_coding_rate(self, coding_rate: int) -> None:
         """
         Coding rate 4/x
         """
+        self._coding_rate = coding_rate
         self._set_lora_params({"coding_rate": coding_rate})
 
     def set_preamble_length(self, bits: int) -> None:
+        self._preamble_length = bits
         self._set_lora_params({"preamble_length": bits})
 
     def set_syncword(self, syncword: int) -> None:
@@ -68,6 +137,8 @@ class LoraModem:
         invert_iq: bool,
         low_data_rate_optimize: bool,
     ):
+        self._crc = crc
+        self._low_data_rate_optimize = low_data_rate_optimize
         self._set_lora_params({
             "crc": crc,
             "invert_iq": invert_iq,
