126db43862291335860562ec40fc6a8ce9c6e172
theverygaming <18639279+theverygaming@users.noreply.github.com>
2026-02-28 19:01:54 +0100

    always call WiFi.begin

--- lora_modem/src/config.cpp
+++ lora_modem/src/config.cpp
@@ -62,8 +62,11 @@ void config_init() {
         LOG_INFO("no config file found, creating new one");
         config_reset();
     }
-
-#if defined(ESP32) && defined(USE_WIFI)
+#ifdef USE_WIFI
+    // on some platforms (namely ESP32-C6) it seems initializing a WiFiServer
+    // without calling WiFi.begin() is a really bad idea (crash!)
+    WiFi.begin();
+#if defined(ESP32)
     WiFi.onEvent([](WiFiEvent_t event, WiFiEventInfo_t info){
         static WiFiEvent_t ev_prev = (WiFiEvent_t)0;
         if (event == ev_prev) {
@@ -103,6 +106,7 @@ void config_init() {
                 break;
         }
     });
+#endif
 #endif
 
     config_refresh();
