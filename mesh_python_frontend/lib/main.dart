2e161bd711831cfb85691a325f77eb649225c2be
theverygaming <18639279+theverygaming@users.noreply.github.com>
2026-03-02 00:16:10 +0100

    cluster map markers so the map doesn't lag lmao

--- mesh_python_frontend/lib/main.dart
+++ mesh_python_frontend/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
+import 'package:flutter_map_marker_cluster/flutter_map_marker_cluster.dart';
 import 'package:latlong2/latlong.dart';
 import 'api.dart' as api;
 
@@ -136,19 +137,44 @@ class _MyHomePageState extends State<MyHomePage> {
                     FutureBuilder<List<api.Node>>(
                       future: futureNodes,
                       builder: (context, snapshot) {
+                        List<Marker> markers = [];
                         if (snapshot.hasData) {
-                          return MarkerLayer(
-                            markers: snapshot.data!.map((node) => (Marker(
-                              point: LatLng(node.lat ?? 0.0, node.lon ?? 0.0),
-                              width: 10,
-                              height: 10,
-                              child: FlutterLogo(),
-                            ))).toList(),
-                          );
+                          markers = snapshot.data!.map((node) => (Marker(
+                            point: LatLng(node.lat ?? 0.0, node.lon ?? 0.0),
+                            width: 10,
+                            height: 10,
+                            child: FlutterLogo(),
+                          ))).toList();
                         } else if (snapshot.hasError) {}
 
-                        return MarkerLayer(
-                          markers: [],
+                        return MarkerClusterLayerWidget(
+                          options: MarkerClusterLayerOptions(
+                            maxClusterRadius: 45,
+                            size: const Size(40, 40),
+                            alignment: Alignment.center,
+                            padding: const EdgeInsets.all(50),
+                            maxZoom: 15,        
+                            markers: markers,
+                            builder: (context, markers) {
+                              return Container(
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(20),
+                                  color: (
+                                    // >= 100 -> red
+                                    // >= 10 -> yellow
+                                    // < 10 -> green
+                                    markers.length < 100 ? (markers.length < 10 ? Colors.green : Colors.yellow) : Colors.red
+                                  ),
+                                ),
+                                child: Center(
+                                  child: Text(
+                                    markers.length.toString(),
+                                    style: const TextStyle(color: Colors.black),
+                                  ),
+                                ),
+                              );
+                            },
+                          ),
                         );
                       }
                     ),
